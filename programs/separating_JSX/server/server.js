9d2471686eee70be69707f92fbf70c4373a7b2d9
gianmillare <gianmillaregsm@gmail.com>
2020-11-30 20:40:34 -0800

    completed practice chapter 2; debugged issue in package.json preventing npm start

--- programs/separating_JSX/server/server.js
+++ programs/separating_JSX/server/server.js
@@ -6,10 +6,10 @@ const app = express();
 
 // step 3: run the server using one of the below options
 // Option 1: fileServerMiddleWare
-// const fileServerMiddleWare = express.static('public');
-// app.use('/', fileServerMiddleWare);
+const fileServerMiddleWare = express.static('public');
+app.use('/', fileServerMiddleWare);
 // Option 2: app.use and combine the above code into one line
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // step 4: launch the app on localhost 3000
 app.listen(3000, function() {
