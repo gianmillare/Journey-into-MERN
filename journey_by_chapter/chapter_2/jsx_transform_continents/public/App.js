396f568ab7594daaeadb6793fb67eac9ee53f5d0
gianmillare <gianmillaregsm@gmail.com>
2020-12-02 23:41:39 -0800

    completed the jsk transform continents app

--- journey_by_chapter/chapter_2/jsx_transform_continents/public/App.js
+++ journey_by_chapter/chapter_2/jsx_transform_continents/public/App.js
@@ -1,4 +1,12 @@
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const greetings = [];
+
+for (var i = 0; i < continents.length; i++) {
+  greetings.push(`Hello ${continents[i]}`);
+}
+
+const message = greetings.join(' ');
 const element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello Continents!"));
+}, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
--- journey_by_chapter/chapter_2/jsx_transform_continents/src/App.jsx
+++ journey_by_chapter/chapter_2/jsx_transform_continents/src/App.jsx
@@ -1,6 +1,15 @@
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const greetings = [];
+
+for (var i = 0; i < continents.length; i++) {
+    greetings.push(`Hello ${continents[i]}`)
+}
+
+const message = greetings.join(' ');
+
 const element = (
     <div title='Outer div'>
-        <h1>Hello Continents!</h1>
+        <h1>{message}</h1>
     </div>
 );
 
